8443305bff8d06d321f0a49db1c19e4e8ad8ef30
WuCris <chris_home@live.ca>
2016-10-09 18:28:09 -0400

    add agree to tos and email variable

--- rootfs/etc/cont-init.d/init.sh
+++ rootfs/etc/cont-init.d/init.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/with-contenv sh
 
+
+# If TOS isn't agreed to output error to stderr
+if [ ! $AGREE_TO_TOS = yes ]; then
+  echo "FAILURE: Let's Encrypt Terms of Service hasn't been agreed to. EXITING..." 1>&2
+  exit 111
+fi
+
+# If email not set output error to stderr
+if [ -z $EMAIL_ACCOUNT ]; then
+  echo "FAILURE: Email for Let's Encypt not set. EXITING..." 1>&2
+  exit 222
+fi
+
+
 # This takes all environment variables with DOMAIN_ prefix and prepends a -d flag
 # and a www. infront of it (while also preserving non www domains)
 DOMAINS=$(env | awk 'BEGIN{FS="="} /DOMAIN_/ {print " -d " $2  " -d www."$2} ORS=" " FS="="')
 
-certbot certonly --webroot -w /var/certbot-webroot/ $DOMAINS
+certbot certonly --agree-tos --account $EMAIL_ACCOUNT --webroot -w /var/certbot-webroot/ $DOMAINS
