230f257ee7b7dceaa93a51076b279951ac302729
WuCris <chris_home@live.ca>
2016-10-12 01:03:24 -0400

    f

--- rootfs/etc/cont-init.d/init.sh
+++ rootfs/etc/cont-init.d/init.sh
@@ -47,4 +47,4 @@ exit_on_certbot_fail ()
 # This takes all environment variables with DOMAIN_ prefix and prepends a -d flag
 # and a www. infront of it (while also preserving non www domains)
 DOMAINS=$(env | awk 'BEGIN{FS="="} /DOMAIN_/ {print " -d " $2  " -d www."$2} ORS=" " FS="="')
-certbot certonly --agree-tos --account $EMAIL_ACCOUNT --webroot -w /var/certbot-webroot/ $DOMAINS || exit_on_certbot_fail
+certbot certonly --agree-tos --email $EMAIL_ACCOUNT --webroot -w /var/certbot-webroot/ $DOMAINS || exit_on_certbot_fail
